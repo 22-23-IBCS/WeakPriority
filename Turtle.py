802e17017725a9aacb2e15893bc9f027113a8cb0
Finn Keogh <Finn_Keogh@m018766.aw.org>
2022-09-30 12:13:05 -0700

    Turtle drawing

---
+++ Turtle.py
@@ -0,0 +1,122 @@
+Python 3.10.3 (v3.10.3:a342a49189, Mar 16 2022, 09:34:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+Traceback (most recent call last):
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 86, in <module>
+    main()
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 81, in main
+    art.Move(150, 0)
+AttributeError: 'Artist' object has no attribute 'Move'. Did you mean: 'move'?
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+Traceback (most recent call last):
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 87, in <module>
+    main()
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 83, in main
+    art.Smile(50)
+AttributeError: 'Artist' object has no attribute 'Smile'
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+Traceback (most recent call last):
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 85, in <module>
+    main()
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 79, in main
+    art.Smile(50)
+AttributeError: 'Artist' object has no attribute 'Smile'
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+Traceback (most recent call last):
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 101, in <module>
+    main()
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 93, in main
+    art.Wink(50)
+  File "/Users/Finn_Keogh/Desktop/WeakPriority/Artist.py", line 50, in Wink
+    self.t.left(1)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/turtle.py", line 1700, in left
+    self._rotate(angle)
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/turtle.py", line 3280, in _rotate
+    self._update()
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/turtle.py", line 2661, in _update
+    self._update_data()
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/turtle.py", line 2647, in _update_data
+    self.screen._incrementudc()
+  File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/turtle.py", line 1293, in _incrementudc
+    raise Terminator
+turtle.Terminator
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
+
+=========== RESTART: /Users/Finn_Keogh/Desktop/WeakPriority/Artist.py ==========
