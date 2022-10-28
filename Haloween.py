b89e796d6278f36328e0ecaa29bf15c8828d2a67
Finn Keogh <Finn_Keogh@m018766.aw.org>
2022-10-28 10:48:16 -0700

    randompath

--- Haloween.py
+++ Haloween.py
@@ -1,113 +1,83 @@
 import random
 
 class House:
-
-    
     def __init__(self):
-        self.neighborhood = [[],[],[],[],[]]
-        for l in self.neighborhood:
-            for i in range(5):
-                l.append(random.randint(1, 10))
+        self.rating = random.randint(1, 10)
 
-    def getHouse(self, x, y):
-        return self.neighborhood[x][y]
+    def getRating(self):
+        return self.rating
 
-    def getNeighborhood(self):
-        return self.neighborhood
+def randPath(m, num):
+
+    p = []
     
-    
-        
+    while(len(p) < num):
 
+        x = random.randint(0,4)
+        y = random.randint(0,4)
+        p.append([x,y])
+        pVal = m[x][y]
+
+        for i in range(num - 1):
+          
+
+            neighbors = [[x+1,y], [x-1,y], [x,y+1], [x, y-1]]
+            bad = []
+            for n in neighbors:
+                if (n[0] > 4) or (n[0] < 0):
+                    bad.append(n)
+                elif (n[1] > 4) or (n[1] < 0):
+                    bad.append(n)
+                if n in p:
+                    bad.append(n)
+
+            for b in bad:
+                neighbors.remove(b)
+            if len(neighbors) == 0:
+                break
+
+                        
+            randN = random.choice(neighbors)
+            p.append(randN)
+            x = randN[0]
+            y = randN[1]
+            pVal = pVal + m[x][y]      
+
+    return pVal, p
+            
+                
 
 
 def main():
-    houses = House()
-    for x in range(len(houses.getNeighborhood())):
-        for y in range(len(houses.getNeighborhood()[x])):#length of list at spot x
-            print(houses.getNeighborhood()[x][y])
-    print("break 1")
-    for x in range(len(houses.getNeighborhood())):
-        l = []
-        l.append(houses.getNeighborhood()[x][0])
-        l.append(houses.getNeighborhood()[x][1])
-        l.append(houses.getNeighborhood()[x][2])
-        l.append(houses.getNeighborhood()[x][3])
-        l.append(houses.getNeighborhood()[x][4])
-        x = x+1
-        print(l)
-    print("break 2")
-    maximum = max(l)
-    for x in range(0,4): #Find starting position(biggest value)
-        if maximum == l[x][0]:
-            yPos = x
-            break
-        if maximum == l[x][1]:
-            yPos = x
-            break
-        if maximum == l[x][2]:
-            yPos = x
-            break
-        if maximum == l[x][3]:
-            yPos = x
-            break
-        if maximum == l[x][4]:
-            yPos = x
-            break
-    print(yPos)
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-
-    maximum = max(l)
-    print(maximum)
-    print("break 3")
-    for i in range (0,len(l)):
-        if (l[i]==maximum):
-            break
-    if(i<=4):
-        x=0
-        y=i
-    if(4<i<=9):
-        x=1
-        y=i-5
-    if(9<i<=14):
-        x=2
-        y=i-10
-    if(14<i<=19):
-        x=3
-        y=i-15
-    if(19<i<=24):
-        x=4
-        y=i-20
-    print("break 4")
-    print(i)
-    print(x,y)
-    print(houses.getHouse(x,y))
     
-            
+    m = [[], [], [], [], []]
+    for l in m:
+        
+        for i in range(5):
+            h = House()
+            l.append(h.getRating())
+    for i in range(5):
+        print(m[0][i], m[1][i], m[2][i], m[3][i], m[4][i])
+
+    num = int((input("How many houses?\n")))  
     
-'''
-     
+    total = 0
+    for i in range(5):
+        for j in range(5):
+            total = total + m[i][j]
+
+    average = total/25
+
+    pVal, p = randPath(m, num)
+    while (average > pVal/num):
+        pVal, p = randPath(m, num)
+
+    print(p)
+
+    print("average value in path: " + str(pVal/num))
+    print("average value in neighborhood: " + str(average))
+
+
 
 if __name__=="__main__":
     main()
